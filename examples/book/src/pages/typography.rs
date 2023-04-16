272a64225ec463e15383d8ba5362de3c5206101a
Lukas Potthast <privat@lukas-potthast.de>
2023-04-16 15:37:25 +0200

    Add typography examples

--- examples/book/src/pages/typography.rs
+++ examples/book/src/pages/typography.rs
@@ -1,8 +1,19 @@
+use leptonic::prelude::*;
 use leptos::*;
 
 #[component]
 pub fn PageTypography(cx: Scope) -> impl IntoView {
     view! { cx,
-        <h2>"Typography"</h2>
+        <Typography variant=TypographyVariant::H2>"Typography"</Typography>
+
+        <Separator />
+
+        <Typography variant=TypographyVariant::H1>"Typography - H1"</Typography>
+        <Typography variant=TypographyVariant::H2>"Typography - H2"</Typography>
+        <Typography variant=TypographyVariant::H3>"Typography - H3"</Typography>
+        <Typography variant=TypographyVariant::H4>"Typography - H4"</Typography>
+        <Typography variant=TypographyVariant::H5>"Typography - H5"</Typography>
+        <Typography variant=TypographyVariant::H6>"Typography - H6"</Typography>
+        <Typography variant=TypographyVariant::Code>"Typography - Code"</Typography>
     }
 }
