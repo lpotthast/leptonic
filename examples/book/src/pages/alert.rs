98e8636416d5f6a7cff325e7a505e0375004cd34
Lukas Potthast <privat@lukas-potthast.de>
2023-06-10 07:39:37 +0200

    Make the alert variant a MaybeSignal

--- examples/book/src/pages/alert.rs
+++ examples/book/src/pages/alert.rs
@@ -3,14 +3,17 @@ use leptos::*;
 
 #[component]
 pub fn PageAlert(cx: Scope) -> impl IntoView {
-    let (center_alert, set_center_alert) = create_signal(cx, false);
+    let (centered, set_centered) = create_signal(cx, false);
     view! { cx,
         <H2>"Alerts"</H2>
 
-        <Button on_click=move |_| set_center_alert.update(|it| *it = !*it)>"Center toggle"</Button>
-        <Alert variant=AlertVariant::Success title="asd" centered=center_alert>"Success alert"</Alert>
-        <Alert variant=AlertVariant::Info title="asd" centered=true>"Info alert"</Alert>
-        <Alert variant=AlertVariant::Warn title="asd">"Warn alert"</Alert>
-        <Alert variant=AlertVariant::Danger title="asd">"Danger alert"</Alert>
+        <Alert variant=AlertVariant::Success title=move |_cx| "Success" centered=centered>"Action completed."</Alert>
+        <Alert variant=AlertVariant::Info title=move |_cx| "Info" centered=centered>"This concept is based on [...]"</Alert>
+        <Alert variant=AlertVariant::Warn title=move |_cx| "Warn" centered=centered>"This seems not plausible."</Alert>
+        <Alert variant=AlertVariant::Danger title=move |_cx| "Danger" centered=centered>"There was an error!"</Alert>
+
+        <P>"Alerts can be dynamically centered using a signal."</P>
+
+        <Button on_click=move |_| set_centered.update(|it| *it = !*it)>"Center toggle"</Button>
     }
 }
--- src/alert.rs
+++ src/alert.rs
@@ -22,25 +22,33 @@ impl AlertVariant {
     }
 }
 
+// TODO: Use Slots for title and body when available.
+// TODO: Icon (or entire prepend) must be overridable.
 #[component]
-pub fn Alert<T>(
+pub fn Alert<T, V>(
     cx: Scope,
-    variant: AlertVariant,
+    #[prop(into)] variant: MaybeSignal<AlertVariant>,
     title: T,
     #[prop(into, optional)] centered: OptionalMaybeSignal<bool>,
     children: Children,
 ) -> impl IntoView
 where
-    T: IntoView + 'static,
+    T: Fn(Scope) -> V + 'static,
+    V: IntoView + 'static,
 {
     view! { cx,
-        <leptonic-alert variant=variant.to_str()>
+        <leptonic-alert variant=move || variant.get().to_str()>
             <div class="prepend">
-                <Icon icon=BsIcon::BsCheckCircleFill />
+                {move || match variant.get() {
+                    AlertVariant::Success => view!{cx, <Icon icon=BsIcon::BsCheckCircleFill /> },
+                    AlertVariant::Info => view!{cx, <Icon icon=BsIcon::BsInfoCircleFill /> },
+                    AlertVariant::Warn => view!{cx, <Icon icon=BsIcon::BsExclamationCircleFill /> },
+                    AlertVariant::Danger => view!{cx, <Icon icon=BsIcon::BsExclamationTriangleFill /> },
+                }}
             </div>
-            <div class="content" class:centered=centered.0.as_ref().map(|it| it.get()).unwrap_or(false)>
+            <div class="content" class:centered=move || centered.0.as_ref().map(|it| it.get()).unwrap_or(false)>
                 <div class="title">
-                    { title.into_view(cx) }
+                    { move || title(cx) }
                 </div>
                 { children(cx) }
             </div>
