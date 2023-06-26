b7ec80a28c1ac0850799134f276263164e407e2f
Lukas Potthast <privat@lukas-potthast.de>
2023-06-26 16:05:12 +0200

    Add icons; Lock brand-color; Update button styling

--- examples/book/src/pages/err404.rs
+++ examples/book/src/pages/err404.rs
@@ -8,10 +8,18 @@ pub fn PageErr404(cx: Scope) -> impl IntoView {
     view! { cx,
         <H1>"404 Not Found"</H1>
 
-        <Link href=AppRoutes::Welcome>
-            <Button on_click=move |_| {} variant=ButtonVariant::Filled>
-                "Back"
-            </Button>
-        </Link>
+        <div class="err-404">
+            <div class="info">
+                <H1 id="error">"404"</H1>
+                <H2 id="whoops">"Whoops, this page doesn’t exist :-("</H2>
+
+                <Link href=AppRoutes::Welcome>
+                    <Button on_click=move |_| {} variant=ButtonVariant::Filled>
+                        "Back"
+                    </Button>
+                </Link>
+            </div>
+            <img id="ferris" src="/res/icon/ferris-panic.svg" alt="Panicked ferris (crab, logo of Rust)" />
+        </div>
     }
 }
