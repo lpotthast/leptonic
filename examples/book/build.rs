cd1a674b567e4085e828aa7e49e9ecf5dcb9b2e9
Lukas Potthast <privat@lukas-potthast.de>
2023-06-28 00:29:29 +0200

    Simplify build.rs script

--- examples/book/build.rs
+++ examples/book/build.rs
@@ -1,43 +1,26 @@
 use std::io::Write;
 use std::path::{Path, PathBuf};
 
-const JS_DIR: &str = "./generated/js";
-
 pub fn main() {
     let root: std::path::PathBuf = std::env::var("CARGO_MANIFEST_DIR").unwrap().into();
-    leptonic_theme::generate(root.join("generated").join("leptonic"));
+    let generated_dir = root.join("generated");
+    let js_dir = generated_dir.join("js");
+
+    leptonic_theme::generate(generated_dir.join("leptonic"));
     println!("cargo:warning=theme written");
 
-    make_dir(Path::new(JS_DIR));
+    std::fs::create_dir_all(js_dir.clone()).unwrap();
     println!("cargo:warning=js dir created");
 
-    write_str(
-        leptos_tiptap_build::TIPTAP_BUNDLE_MIN_JS,
-        path(JS_DIR, "tiptap-bundle.min.js"),
-    );
+    std::fs::File::create(js_dir.join("tiptap-bundle.min.js"))
+        .unwrap()
+        .write(leptos_tiptap_build::TIPTAP_BUNDLE_MIN_JS.as_bytes())
+        .unwrap();
     println!("cargo:warning=tiptap-bundle.min.js written");
 
-    write_str(leptos_tiptap_build::TIPTAP_JS, path(JS_DIR, "tiptap.js"));
+    std::fs::File::create(js_dir.join("tiptap.js"))
+        .unwrap()
+        .write(leptos_tiptap_build::TIPTAP_JS.as_bytes())
+        .unwrap();
     println!("cargo:warning=tiptap.js written");
-}
-
-fn path<'a>(root: &'static str, path: &'a str) -> PathBuf {
-    PathBuf::from(format!("{root}/{path}"))
-}
-
-fn make_dir(path: &Path) {
-    println!("Creating directory: {}", path.display());
-    std::fs::create_dir_all(path).unwrap();
-}
-
-fn write_str(src: impl AsRef<str>, path: impl AsRef<Path>) {
-    let src = src.as_ref();
-    let path = path.as_ref();
-    println!(
-        "Writing {path} (len = {len})",
-        len = src.len(),
-        path = path.display(),
-    );
-    let mut file = std::fs::File::create(path).unwrap();
-    file.write(src.as_bytes()).unwrap();
 }
