b5525e2a5861c49d2823f826a129a69d22b8d8e5
Lukas Potthast <privat@lukas-potthast.de>
2023-07-22 16:47:03 +0200

    Use Grid on 404 page; Update 404 page styling

--- examples/book/src/pages/err404.rs
+++ examples/book/src/pages/err404.rs
@@ -6,20 +6,22 @@ use crate::app::AppRoutes;
 #[component]
 pub fn PageErr404(cx: Scope) -> impl IntoView {
     view! { cx,
-        <H1>"404 Not Found"</H1>
+        <Grid spacing=Size::Zero class="err-404">
+            <Row>
+                <Col sm=6 xs=12 class="info" h_align=ColAlign::Center>
+                    <H1 id="error">"404"</H1>
+                    <H2 id="whoops">"Whoops, this page doesn't exist :-("</H2>
 
-        <div class="err-404">
-            <div class="info">
-                <H1 id="error">"404"</H1>
-                <H2 id="whoops">"Whoops, this page doesn’t exist :-("</H2>
-
-                <Link href=AppRoutes::Welcome>
-                    <Button on_click=move |_| {} variant=ButtonVariant::Filled>
-                        "Back"
-                    </Button>
-                </Link>
-            </div>
-            <img id="ferris" src="/res/icon/ferris-panic.svg" alt="Panicked ferris (crab, logo of Rust)" />
-        </div>
+                    <Link href=AppRoutes::Welcome>
+                        <Button id="back-btn" on_click=move |_| {} variant=ButtonVariant::Filled>
+                            "Back"
+                        </Button>
+                    </Link>
+                </Col>
+                <Col sm=6 xs=12 h_align=ColAlign::Center>
+                    <img id="ferris" src="/res/icon/ferris-panic_transparent.svg" alt="Ferris (Rust mascot, a crab) panicked" />
+                </Col>
+            </Row>
+        </Grid>
     }
 }
