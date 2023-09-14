2b01ba039fccb46af9842f1058ebdb94421a2ecc
Lukas Potthast <privat@lukas-potthast.de>
2023-09-14 16:04:04 +0200

    Use tracing-wasm dep from crates.io

--- examples/book/src/main.rs
+++ examples/book/src/main.rs
@@ -2,21 +2,16 @@ use leptos::*;
 
 mod app;
 mod pages;
-mod wasm_tracing_layer;
 
 use crate::app::*;
 
 fn main() {
     console_error_panic_hook::set_once();
-    //std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-
-    // Initialize a tracing subscriber logging to the browser console.
-    wasm_tracing_layer::set_as_global_default_with_config(
-        wasm_tracing_layer::WASMLayerConfigBuilder::default()
+    tracing_wasm::set_as_global_default_with_config(
+        tracing_wasm::WASMLayerConfigBuilder::default()
             .set_max_level(tracing::Level::DEBUG)
             .build(),
     );
-
     mount_to_body(|| {
         view! { <App/> }
     })
