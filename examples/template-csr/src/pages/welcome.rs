01d15a2d3f4524bc3477e2fcc35dd74409ab3dd5
Lukas Potthast <privat@lukas-potthast.de>
2024-01-29 20:13:35 +0100

    Update templates

--- examples/template-csr/src/pages/welcome.rs
+++ examples/template-csr/src/pages/welcome.rs
@@ -6,7 +6,7 @@ pub fn Welcome() -> impl IntoView {
     let (count, set_count) = create_signal(0);
 
     view! {
-        <Box style="display: flex; flex-direction: column; align-items: center; padding: 1em; max-width: 10em; min-width: 100%">
+        <Box style="display: flex; flex-direction: column; align-items: center; padding: 1em; min-height: 100%; min-width: 100%">
             <H2>"Welcome to Leptonic"</H2>
 
             <span style="margin-top: 3em;">"Count: " {move || count.get()}</span>
--- examples/template-ssr/src/pages/welcome.rs
+++ examples/template-ssr/src/pages/welcome.rs
@@ -6,14 +6,13 @@ pub fn Welcome() -> impl IntoView {
     let (count, set_count) = create_signal(0);
 
     view! {
-
-        <div style="display: flex; flex-direction: column; align-items: center; padding: 1em; max-width: 10em; min-width: 100%">
+        <Box style="display: flex; flex-direction: column; align-items: center; padding: 1em; min-height: 100%; min-width: 100%">
             <H2>"Leptonic template SSR"</H2>
 
             <span style="margin-top: 3em;">"Count: " {move || count.get()}</span>
             <Button on_click=move|_| set_count.update(|c| *c += 1)>
                 "Increase"
             </Button>
-        </div>
+        </Box>
     }
 }
