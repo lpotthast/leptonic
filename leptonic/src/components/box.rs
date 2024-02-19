03387aaec94217973b06f17e3de58bcfd999be36
Lukas Potthast <privat@lukas-potthast.de>
2024-02-19 15:52:07 +0100

    Allow additional attributes on <Box>

--- leptonic/src/components/box.rs
+++ leptonic/src/components/box.rs
@@ -5,10 +5,13 @@ pub fn Box(
     #[prop(into, optional)] id: Option<AttributeValue>,
     #[prop(into, optional)] class: Option<AttributeValue>,
     #[prop(into, optional)] style: Option<AttributeValue>,
+    /// Arbitrary additional attributes.
+    #[prop(attrs)]
+    attributes: Vec<(&'static str, Attribute)>,
     children: Children,
 ) -> impl IntoView {
     view! {
-        <leptonic-box id=id class=class style=style>
+        <leptonic-box {..attributes} id=id class=class style=style>
             { children() }
         </leptonic-box>
     }
