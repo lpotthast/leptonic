f03b53bf8bfd4bf5b41b8a73855080166e9126b2
Lukas Potthast <privat@lukas-potthast.de>
2025-01-04 16:33:39 +0100

    Remove unnecessary `Box` component; Fix ssr template

--- leptonic/src/components/mod.rs
+++ leptonic/src/components/mod.rs
@@ -1,6 +1,5 @@
 pub mod alert;
 pub mod app_bar;
-pub mod r#box;
 pub mod button;
 pub mod card;
 pub mod checkbox;
@@ -110,7 +109,6 @@ pub mod prelude {
     pub use super::quicksearch::Quicksearch;
     pub use super::quicksearch::QuicksearchOption;
     pub use super::quicksearch::QuicksearchTrigger;
-    pub use super::r#box::Box;
     pub use super::radio::Radio;
     pub use super::radio::RadioGroup;
     pub use super::root::Leptonic;
