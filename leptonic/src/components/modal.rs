5a2af032e0de4ef5e5e06cd0c8db917edd8f79b5
Lukas Potthast <privat@lukas-potthast.de>
2025-12-19 21:12:51 +0100

    Hide modals when `<Modal>` cleanup fn's run

--- leptonic/src/components/modal.rs
+++ leptonic/src/components/modal.rs
@@ -1,5 +1,3 @@
-use std::sync::Arc;
-
 use leptos::prelude::*;
 use uuid::Uuid;
 
@@ -9,53 +7,52 @@ use crate::{
 };
 
 #[derive(Clone)]
-struct ShownModalData {
-    key: Uuid,
-    children: ChildrenFn,
+struct ShownModal {
+    modal_id: Uuid,
+    modal_renderer: ViewFn,
     on_backdrop_interaction: Option<Callback<(), ()>>,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Clone, Copy)]
 struct ModalRootContext {
-    modals: RwSignal<Vec<Uuid>>,
-
-    shown_modals: RwSignal<Vec<ShownModalData>>,
+    /// List of modals currently shown to the user.
+    shown_modals: RwSignal<Vec<ShownModal>>,
 }
 
 impl ModalRootContext {
-    fn push(&self, data: Uuid) {
-        self.modals.update(move |m| m.push(data));
+    fn is_shown(&self, modal_id: Uuid) -> bool {
+        self.shown_modals
+            .read_untracked()
+            .iter()
+            .any(|it| it.modal_id == modal_id)
     }
 
-    fn remove(&self, key: Uuid) {
-        self.modals.update(move |m| {
-            if let Some(idx) = m.iter().position(|it| *it == key) {
-                m.remove(idx);
-            }
-        });
-    }
+    /// Shows the modal with `modal_id`.
+    ///
+    /// # Panics
+    /// - If the modal is already shown.
+    fn show(&self, data: ShownModal) {
+        assert!(!self.is_shown(data.modal_id));
 
-    fn push_shown(&self, data: ShownModalData) {
         self.shown_modals.update(move |m| m.push(data));
     }
 
-    fn remove_shown(&self, key: Uuid) {
+    /// Removes the modal with `modal_id` from the list of shown modals.
+    /// Should the modal not be shown right now, nothing happens.
+    fn hide(&self, modal_id: Uuid) {
         self.shown_modals.update(move |m| {
-            if let Some(idx) = m.iter().position(|it| it.key == key) {
+            if let Some(idx) = m.iter().position(|it| it.modal_id == modal_id) {
                 m.remove(idx);
             }
         });
+        assert!(!self.is_shown(modal_id));
     }
 }
 
 #[component]
 pub fn ModalRoot(children: Children) -> impl IntoView {
-    let modals = RwSignal::new(Vec::new());
     let shown_modals = RwSignal::new(Vec::new());
-    let ctx = ModalRootContext {
-        modals,
-        shown_modals,
-    };
+    let ctx = ModalRootContext { shown_modals };
     provide_context::<ModalRootContext>(ctx.clone());
 
     let has_modals = Memo::new(move |_| shown_modals.with(|modals| !modals.is_empty()));
@@ -94,10 +91,8 @@ pub fn ModalRoot(children: Children) -> impl IntoView {
             <leptonic-modals>
                 <For
                     each=move || ctx.shown_modals.get()
-                    key=|it| it.key
-                    children=|it| {
-                        view! { {(it.children)()} }
-                    }
+                    key=|it| it.modal_id
+                    children=|it| it.modal_renderer.run()
                 />
             </leptonic-modals>
         </leptonic-modal-host>
@@ -127,37 +122,36 @@ pub fn Modal(
     }
 
     let key = Uuid::now_v7();
-    let shown = Memo::new(move |_| show_when.get());
+    let should_be_shown = Memo::new(move |_| show_when.get());
 
     let id = StoredValue::new(id.unwrap_or_else(|| key.to_string()));
     let class = StoredValue::new(class);
 
-    let modal = Arc::new(move || {
+    let modal_renderer = ViewFn::from(move || {
         view! {
             <leptonic-modal id=id.get_value() class=class.get_value()>
                 { children() }
             </leptonic-modal>
         }
-    }.into_any());
+    });
 
-    Effect::new_isomorphic(move |_| match shown.get() {
-        true => ctx.push_shown(ShownModalData {
-            key,
-            children: modal.clone(),
-            on_backdrop_interaction,
-        }),
-        false => {
-            ctx.remove_shown(key);
+    Effect::new_isomorphic(move |_| {
+        if should_be_shown.get() {
+            ctx.show(ShownModal {
+                modal_id: key,
+                modal_renderer: modal_renderer.clone(),
+                on_backdrop_interaction,
+            });
+        } else {
+            ctx.hide(key);
         }
     });
 
-    ctx.push(key);
-
     on_cleanup(move || {
-        ctx.remove(key);
+        ctx.hide(key);
     });
 
-    view! {}
+    ()
 }
 
 #[component]
@@ -179,9 +173,7 @@ pub fn ModalTitle(children: Children) -> impl IntoView {
 }
 
 #[component]
-pub fn ModalBody(
-    children: Children,
-) -> impl IntoView {
+pub fn ModalBody(children: Children) -> impl IntoView {
     view! {
         <leptonic-modal-body>
             { children() }
